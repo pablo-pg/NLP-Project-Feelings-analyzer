1c2a6dc56d4516dfa1903fe20ee56ee14cdc2847
PabloPG <alu0101318318@ull.edu.es>
2022-04-27 16:42:48 +0100

    workable dataframe

--- main.py
+++ main.py
@@ -2,6 +2,6 @@
 # Autor: Pablo Pérez González (alu0101318318@ull.edu.es)
 
 import pandas as pd
-df = pd.read_excel(r"COV_train.xlsx", sheet_name=0)
+df = pd.read_excel(r"COV_train.xlsx", sheet_name=0, dtype=str)
 df2 = pd.DataFrame(df, columns= ['Message'])
-print(df.index)
+print(df["Emotion"])
