890e821359aaa94f0d10202ab6c5a88dfc01cf20
PabloPG <alu0101318318@ull.edu.es>
2022-04-27 16:40:29 +0100

    basic load of data

---
+++ main.py
@@ -0,0 +1,7 @@
+# Proyecto sobre Procesamiento de Lenguaje Natural para Inteligencia Artificial Avanzada
+# Autor: Pablo Pérez González (alu0101318318@ull.edu.es)
+
+import pandas as pd
+df = pd.read_excel(r"COV_train.xlsx", sheet_name=0)
+df2 = pd.DataFrame(df, columns= ['Message'])
+print(df.index)
