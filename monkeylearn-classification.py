b6f6642d5ebce7e0d5dc36f05faddd12138ee2ea
PabloPG <alu0101318318@ull.edu.es>
2022-05-18 19:34:33 +0100

    Not using monkeylearn

--- monkeylearn-classification.py
+++ monkeylearn-classification.py
@@ -5,7 +5,7 @@
 
 import pandas as pd
 import numpy as np
-from monkeylearn import MonkeyLearn
+# from monkeylearn import MonkeyLearn
 
 
 def normalize(datas): 
@@ -21,13 +21,67 @@ def normalize(datas):
 
 
 
-ml = MonkeyLearn('f280dedd11b4b5c26f6ce78898e69f4cb75650a9')
+# ml = MonkeyLearn('f280dedd11b4b5c26f6ce78898e69f4cb75650a9')
 
 df = pd.read_excel(r"COV_train.xlsx",  sheet_name=0, dtype=str)
 rawMessages = df[['Message']].to_numpy(dtype='str')
-# print (rawMessages[0])
 data = normalize(rawMessages)
-# print (data)
-model_id = 'cl_avq7AcJT'
-result = ml.classifiers.classify(model_id, data)
-print(result.body)
+target = normalize(df[['Emotion']].to_numpy(dtype='str'))
+# MONKEY LEARN
+
+# model_id = 'cl_avq7AcJT'
+# result = ml.classifiers.classify(model_id, data)
+# print(result.body)
+
+# VADER
+
+# import nltk
+# nltk.download('vader_lexicon')
+# from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+# for sentence in data:
+#   sid = SentimentIntensityAnalyzer()
+#   ss = sid.polarity_scores(sentence)
+#   for k in sorted(ss):
+#       print('{0}: {1}, '.format(k, ss[k]), end='')
+#   print()
+
+
+# NLTK
+import nltk
+def punc_clean(text):
+    import string as st
+    a=[w for w in text if w not in st.punctuation]
+    return ''.join(a)
+  
+data = list(map(punc_clean, data))
+# print(data[0])
+
+def remove_stopword(text):
+    stopword=nltk.corpus.stopwords.words('english')
+    stopword.remove('not')
+    a=[w for w in nltk.word_tokenize(text) if w not in stopword]
+    return ' '.join(a)
+# data = data.apply(remove_stopword)
+data = list(map(remove_stopword, data))
+# print(data[0])
+print('Eliminados signos de puntuacion y stopwords')
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectr = TfidfVectorizer(ngram_range=(1,2),min_df=1)
+vectr.fit(data)
+vect_X = vectr.transform(data)
+
+
+from sklearn.linear_model import LogisticRegression
+model = LogisticRegression()
+# clf=model.fit(vect_X,data['sentiment'])
+clf = model.fit(vect_X, target)
+# clf.score(vect_X,data['sentiment'])*100
+clf.score(vect_X, target)*100
+
+print('Modelo entrenado')
+
+result1 = clf.predict(vectr.transform(['I love icecream']))
+result2 = clf.predict(vectr.transform(['My son died yesterday']))
+print(result1, result2)
